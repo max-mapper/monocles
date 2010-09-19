4cacf49719daa5de6656b73c633ba80d78039e08
Jesse Hallett <hallettj@gmail.com>
2010-09-19 16:25:55 -0700

    Reload posts via ajax instead of reloading window.

--- evently/profile/profileReady/selectors/form/submit.js
+++ evently/profile/profileReady/selectors/form/submit.js
@@ -1,30 +1,35 @@
-function() {
-  var that = $$(this);
+function(event) {
   var form = this;
-  var host = window.location.href.split("/")[2]
   var date = new Date();
   var id = date.valueOf()+"a";
   var doc = {
     created_at : date,
-	_id: id,
+        _id : id,
     profile : $$("#profile").profile,
     message : $("[name=message]", form).val(),
-	_attachments:{}
+    _attachments : {}
   };
   
   window.files = window.files || [];
   $.each(window.files, function(i, file) {
     doc._attachments["image" + i + "." + file.match] = {
-  		"content_type": "image\/" + file.match,
-  		"data": file.theGoodPart
-  	}
-  })
+      "content_type": "image\/" + file.match,
+      "data": file.theGoodPart
+    };
+  });
   
   $$(this).app.db.saveDoc(doc, {
     success : function(newDoc) {
+      // Clear post entry form
       $("[name=message]", form).val("");
-	  window.location.reload();
+
+      // Remove image attachments from entry form
+      $('a.deleteattachment').trigger('click');
+
+      // Reload posts
+      $('.items').trigger('show');
     }
   });
-  return false;
-};
+
+  event.preventDefault();
+}
