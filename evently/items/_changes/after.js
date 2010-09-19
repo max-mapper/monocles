49608675262bfffc5f041e5f4b6d908c146dadb2
Jesse Hallett <hallettj@gmail.com>
2010-09-19 01:22:45 -0700

    Custom attributes should be prefixed with "data-".

--- evently/items/_changes/after.js
+++ evently/items/_changes/after.js
@@ -1,5 +1,5 @@
 function(){
 	$("a[href=#showProf]").bind("click",function(){
-		$(this).trigger("show_profile", $(this).attr("profile"));
+		$(this).trigger("show_profile", $(this).attr("data-profile"));
 	});
 }
