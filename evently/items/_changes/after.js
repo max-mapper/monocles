c352db7f2c545a1f17f8b6bd3681759da1ef2784
Tyler Gillies <tjgillies@gmail.com>
2010-09-18 21:00:32 -0700

    changed profile hover to be click activated

--- evently/items/_changes/after.js
+++ evently/items/_changes/after.js
@@ -1,7 +1,4 @@
 function(){
-	$("a[href=#showProf]").bind("click",function(){
-		$(this).trigger("show_profile", $(this).attr("profile"));
-	});
 	$("a.hover").cluetip({local:true});
-	$(".hover_profile").cluetip({local:true, sticky:true});
+	$(".hover_profile").cluetip({local:true, sticky:true, activation:"click"});
 }
