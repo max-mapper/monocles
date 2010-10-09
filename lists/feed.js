c3e9b09c217de76b3ee938528a7d9dc29e9dfe78
Tyler Gillies <tjgillies@gmail.com>
2010-10-08 23:37:09 -0700

    more hacks

--- lists/feed.js
+++ lists/feed.js
@@ -3,6 +3,7 @@ function(head, req){
     var rows = [];
     // !json templates.feed
     Mustache = require("vendor/couchapp/lib/mustache");
+    Rfc3339 = require("vendor/couchapp/lib/rfc3339");
     while(row = getRow()){
       if (row.value.profile && row.value.profile.name === req.query.name && row.value.message.length > 0){
         rows.push(row);
@@ -13,14 +14,15 @@ function(head, req){
     var view = {
       username: req.query.name,
       domain: domain,
-      updated_at: new Date(),
+      updated_at: Rfc3339.convert(new Date()),
       gravatar: rows[0].value.profile.gravatar_url,
       entries: rows.map(function(r){
+        var rand = Math.random();
         return {
           entry_title: r.value.message,
-          entry_url: "http://"+req.headers.Host+"/spora/"+r.value.id,
-          entry_published: r.value.created_at,
-          entry_updated: r.value.created_at,
+          entry_url: "http://"+req.headers.Host+"/spora/"+r.value._id,
+          entry_published: Rfc3339.convert(new Date(r.value.created_at)),
+          entry_updated: Rfc3339.convert(new Date(r.value.created_at)),
           entry_content: r.value.message
         };
       })
