97706ea8096ff604b16e99655fdecf34ec6b94d4
maxogden <max@maxogden.com>
2011-03-26 23:02:34 -0700

    adding xml2js sax parser that parses and stores incoming hub notifications

--- updates/pubsub.js
+++ updates/pubsub.js
@@ -1,17 +1,10 @@
-function(doc, req) {
-  req.body = req.body.replace(/<.*?>/,"");
-  var codez = new XML(req.body);
-  var atom = new Namespace('http://www.w3.org/2005/Atom');
-  var poco = new Namespace('http://portablecontacts.net/spec/1.0');
-  var profile = {
-    nickname: codez..atom::author.atom::name.toString(),
-    url: codez..poco::urls.poco::value.toString(),
-    gravatar_url: codez..atom::logo.toString()
-  }
-  var hostname = codez..atom::link.(@rel == 'alternate')[1].@href.toString().split("/")[2];
-  var message = codez..atom::entry.atom::title.toString();
-  var created_at = new Date();
-  if (profile.gravatar_url.length > 0){
-    return [{_id:req.uuid, created_at:created_at, xml:req.body, profile:profile, message:message, hostname:hostname, type:"pubsub"}, "posted"]
-  };
+function(head, req) {
+  var xml2js = require('vendor/xml2js');
+  var json;
+
+  new xml2js.Parser(function(data) {
+    json = [{_id:req.uuid, created_at: new Date(), data:data, type:"pubsub"}, "posted"];
+  }).parseString(req.body);
+
+  return json;
 }
---
+++ vendor/xml2js.js
@@ -0,0 +1,75 @@
+// https://github.com/maxogden/node-xml2js
+
+var sax = require('./sax');
+
+var Parser = function(callback) {
+    var that = this;
+    this.saxParser = sax.parser(true); // make the sax parser
+
+    this.EXPLICIT_CHARKEY = false; // always use the '#' key, even if there are no subkeys
+    this.resultObject = null;
+
+    var stack = [];
+
+    this.saxParser.onopentag = function(node) {
+        var obj = {};
+        obj['#'] = "";
+        for (var key in node.attributes) {
+            if (node.attributes.hasOwnProperty(key)) {
+                if(typeof obj['@'] === 'undefined') {
+                    obj['@'] = {};
+                }
+                obj['@'][key] = node.attributes[key];
+            }
+        }
+        obj['#name'] = node.name; // need a place to store the node name
+        stack.push(obj);
+    };
+
+    this.saxParser.onclosetag = function() {
+        var obj = stack.pop();
+        var nodeName = obj['#name'];
+        delete obj['#name'];
+        var s = stack[stack.length-1];
+
+        // remove the '#' key altogether if it's blank
+        if(obj['#'].match(/^\s*$/)) {
+            delete obj['#'];
+        }
+        else {
+            // turn 2 or more spaces into one space
+            obj['#'] = obj['#'].replace(/\s{2,}/g, " ").trim();
+
+            // also do away with '#' key altogether, if there's no subkeys
+            // unless EXPLICIT_CHARKEY is set
+            if( Object.keys(obj).length == 1 && '#' in obj && !(that.EXPLICIT_CHARKEY) ) {
+                obj = obj['#'];
+            }
+        }
+        
+        // set up the parent element relationship
+        if (stack.length > 0) {
+            if (typeof s[nodeName] === 'undefined')
+                s[nodeName] = obj;
+            else if (s[nodeName] instanceof Array)
+                s[nodeName].push(obj);
+            else {
+                var old = s[nodeName];
+                s[nodeName] = [old];
+                s[nodeName].push(obj);
+            }
+        }
+        else {
+            callback(obj);
+        }
+    };
+
+    this.saxParser.ontext = this.saxParser.oncdata = function(t) {
+        var s = stack[stack.length-1];
+        if(s) { 
+            s['#'] += t;
+        }
+    }
+};
+Parser.prototype.parseString = function(str) { this.saxParser.write(str.toString()); };
+exports.Parser = Parser;
