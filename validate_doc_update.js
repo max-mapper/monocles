001aa24dbfe343f88a7b90960e43644dc2e33e3f
maxogden <max@maxogden.com>
2011-03-27 23:00:55 -0700

    add a validate doc update function

---
+++ validate_doc_update.js
@@ -0,0 +1,19 @@
+function ( newDoc, oldDoc, userCtx, secObj ) {
+  var v = require( "vendor/validate" ).init( newDoc, oldDoc, userCtx, secObj );
+  
+  if ( v.isAdmin() ) return;
+  
+  if( ! userCtx.name ) {
+    // CouchDB sets userCtx.name only after a successful authentication
+    v.unauthorized( "Please log in first." );
+  }
+  
+  if( oldDoc && oldDoc.profile.name !== userCtx.name ) {
+    v.unauthorized( "You are not the author" );  
+  }
+  
+  if ( ( oldDoc && oldDoc.type === "comment" ) || ( oldDoc && oldDoc.type === "note" ) ) {
+    v.unauthorized( "You can't edit that" );
+  }
+  
+}
